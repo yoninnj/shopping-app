cdaf2f092a751673eafe4b919d39e400c921c26a
Yogesh Niranjan <76064277+yoninnj@users.noreply.github.com>
2022-03-12 14:30:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI = 'mongodb+srv://@cluster0.fy6rw.mongodb.net/shop?retryWrites=true&w=majority';
+
+const app = express();
+const store = new MongoDBStore({
+  uri:MONGODB_URI,
+  collection:'sessions'
+});
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({secret: 'a long string that helps in hashing',resave: false, saveUninitialized: false,store:store})
+);
+
+app.use((req, res, next) => {
+  User.findById("60e2f5db6a7bcafde0cadcd6")
+    .then(user => {
+      req.user =user;
+      next();
+    })
+    .catch(err => console.log(err));
+
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+mongoose
+  .connect(MONGODB_URI)
+  .then(result=>{
+    User.findOne().then(user=>{
+      if(!user){
+        const user = new User({
+          name: 'Sumit',
+          email:'sky@gmail.com',
+          cart:{
+            items:[]
+          }
+        })
+        user.save();
+      }
+    })
+    app.listen(3000);
+  })
+  .catch(err=>{
+    console.log(err);
+  })
+
